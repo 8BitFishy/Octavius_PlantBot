786390ee616d079fabf2ee332d529a4203cf38b1
8BitFishy <sowerbutts.pg@hotmail.com>
2023-12-02 17:21:04 +0000

    Testing video

--- Octavius_Plantbot.py
+++ Octavius_Plantbot.py
@@ -53,81 +53,85 @@ class PlantBot():
                 except:
                     command[i] = float(command[i])
 
+        try:
+            if len(command) == 1:
+                if action == "HELLO":
+                    self.telegram_manager.Send_Message("Hello, what can I do for you?")
 
-        if len(command) == 1:
-            if action == "HELLO":
-                self.telegram_manager.Send_Message("Hello, what can I do for you?")
+                elif action == 'TALK':
+                    self.command_manager.Talk(self.telegram_manager)
 
-            elif action == 'TALK':
-                self.command_manager.Talk(self.telegram_manager)
+                elif action == "REBOOT":
+                    self.command_manager.Reboot(self.telegram_manager)
 
-            elif action == "REBOOT":
-                self.command_manager.Reboot(self.telegram_manager)
+                elif action == "DELETE":
+                    self.command_manager.Delete(command, self.telegram_manager)
 
-            elif action == "DELETE":
-                self.command_manager.Delete(command, self.telegram_manager)
+                elif action == "LENGTH":
+                    self.command_manager.File_Length(command, self.telegram_manager)
 
-            elif action == "LENGTH":
-                self.command_manager.File_Length(command, self.telegram_manager)
+                elif action == "UPDATE":
+                    self.command_manager.Update(self.telegram_manager)
 
-            elif action == "UPDATE":
-                self.command_manager.Update(self.telegram_manager)
+                elif action == "DOWNLOAD":
+                    self.command_manager.Download(command, self.telegram_manager)
 
-            elif action == "DOWNLOAD":
-                self.command_manager.Download(command, self.telegram_manager)
+                elif action == "PHOTO" or action == "PIC":
+                    try:
+                        print(f"{ctime()} - Accessing camera to take picture")
+                        image_file = self.camera_manager.Take_Picture()
+                        self.telegram_manager.Send_Image(image_file)
+                        print(f"{ctime()} - Sending picture")
 
-            elif action == "PHOTO" or action == "PIC":
-                try:
-                    print(f"{ctime()} - Accessing camera to take picture")
-                    image_file = self.camera_manager.Take_Picture()
-                    self.telegram_manager.Send_Image(image_file)
-                    print(f"{ctime()} - Sending picture")
-
-                except Exception as E:
-                    self.command_manager.Handle_Error(E, self.telegram_manager)
-
-
-        elif len(command) == 2:
-
-            if action == "PRINT" and command[1] == "files":
-                self.command_manager.Print_Files(self.telegram_manager)
-
-            elif action == "VIDEO" and isinstance(command[1], int):
-
-                try:
-                    print(f"{ctime()} - Accessing camera to record {command[1]} second video")
-                    video_file, output = self.camera_manager.Take_Video(command[1])
-                    self.telegram_manager.Send_Video(video_file)
-                    print(f"{ctime()} - Sending video")
-
-                except Exception as E:
-                    self.command_manager.Handle_Error(E, self.telegram_manager)
+                    except Exception as E:
+                        self.command_manager.Handle_Error(E, self.telegram_manager)
 
 
-        elif len(command) == 3:
+            elif len(command) == 2:
 
-            if action == "PRINT" and command[1] != "files":
-                self.command_manager.Print_File_Contents(command, self.telegram_manager)
+                if action == "PRINT" and command[1] == "files":
+                    self.command_manager.Print_Files(self.telegram_manager)
 
-            elif command[2].lower() == "on" or command[2].lower() == "off":
-                print(ctime() + f" - Action - {command[0].lower()} {command[1]} {command[2]}")
-                self.telegram_manager.Send_Message(f"Turning plug {command[0].lower()} {command[1]} {command[2]}")
-                result = self.rf_manager.Code_Picker(target=str(command[0]).lower(), plug=command[1], action=command[2].lower())
+                elif action == "VIDEO" and isinstance(command[1], int):
 
-                if result == 0:
-                    print(f"{ctime()} - Binary code transmitted")
-                    self.telegram_manager.Send_Message(f"Plug {command[0].lower()} {command[1]} turned {command[2]}")
-                elif result == 1:
-                    print(f"{ctime()} - No binary codes available, could not transmit")
-                    self.telegram_manager.Send_Message(f"No binary codes found, please check files")
-                elif result == 2:
-                    print(f"{ctime()} - Requested target not found")
-                    self.telegram_manager.Send_Message(f"Requested target not found")
-                elif result == 3:
-                    print(f"{ctime()} - Unknown error")
-                    self.telegram_manager.Send_Message(f"Unknown error")
+                    try:
+                        print(f"{ctime()} - Accessing camera to record {command[1]} second video")
+                        video_file, output = self.camera_manager.Take_Video(command[1])
+                        self.telegram_manager.Send_Video(video_file)
+                        print(f"{ctime()} - Sending video")
 
-        else:
+                    except Exception as E:
+                        self.command_manager.Handle_Error(E, self.telegram_manager)
+
+
+            elif len(command) == 3:
+
+                if action == "PRINT" and command[1] != "files":
+                    self.command_manager.Print_File_Contents(command, self.telegram_manager)
+
+                elif command[2].lower() == "on" or command[2].lower() == "off":
+                    print(ctime() + f" - Action - {command[0].lower()} {command[1]} {command[2]}")
+                    self.telegram_manager.Send_Message(f"Turning plug {command[0].lower()} {command[1]} {command[2]}")
+                    result = self.rf_manager.Code_Picker(target=str(command[0]).lower(), plug=command[1], action=command[2].lower())
+
+                    if result == 0:
+                        print(f"{ctime()} - Binary code transmitted")
+                        self.telegram_manager.Send_Message(f"Plug {command[0].lower()} {command[1]} turned {command[2]}")
+                    elif result == 1:
+                        print(f"{ctime()} - No binary codes available, could not transmit")
+                        self.telegram_manager.Send_Message(f"No binary codes found, please check files")
+                    elif result == 2:
+                        print(f"{ctime()} - Requested target not found")
+                        self.telegram_manager.Send_Message(f"Requested target not found")
+                    elif result == 3:
+                        print(f"{ctime()} - Unknown error")
+                        self.telegram_manager.Send_Message(f"Unknown error")
+
+            else:
+                print(ctime() + " - No action - Command not recognised")
+                self.telegram_manager.Send_Message("Command not recognised")
+
+        except Exception as E:
             print(ctime() + " - No action - Command not recognised")
             self.telegram_manager.Send_Message("Command not recognised")
 
